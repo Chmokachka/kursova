2b8056cf084f81bc42fd7255fbebdae2dc972edc
Mac <mac@Macs-MacBook-Pro.local>
2022-04-29 23:37:40 +0300

    little revision

--- MM_13/user_data.sh
+++ MM_13/user_data.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 apt update -y
+apt upgrade -y
 apt install git -y
+apt install python3-pip -y
+apt install python3-venv -y
+git clone https://github.com/Chmokachka/kursova.git
+chmod +x kursova/MM_13/start
+sudo mv kursova/MM_13/page.service /usr/lib/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl start page.service
