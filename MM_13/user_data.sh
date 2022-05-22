72df6714359e6f056b7b51af07f8986f50693faf
Mac <mac@Macs-MacBook-Pro.local>
2022-05-22 13:43:16 +0300

    change service

--- MM_13/user_data.sh
+++ MM_13/user_data.sh
@@ -6,5 +6,6 @@ sudo apt install python3-venv -y
 git clone https://github.com/Chmokachka/kursova.git
 chmod +x kursova/MM_13/start
 sudo mv kursova/MM_13/page.service /usr/lib/systemd/system/
+sudo mv kursova /home/ubuntu
 sudo systemctl daemon-reload
 sudo systemctl start page.service
